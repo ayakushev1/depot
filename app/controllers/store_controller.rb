9f2da8993bb5bd91cd4628a98b00b46446ad417f
ayakushev1 <ayakushev@rambler.ru>
2013-04-17 20:47:40 +0400

    Task F finished

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,9 +1,17 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class StoreController < ApplicationController
   include CurrentCart
-  before_action :set_visit_store_index_page_counter, only: [:index]
-
+  before_action :set_cart
   def index
     @products = Product.order(:title)
-#    @visit_store_index_page_counter = set_visit_store_index_page_counter
   end
 end
+
+
